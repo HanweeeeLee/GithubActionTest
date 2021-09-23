a201346b260aae61205814fab0aff4fd8627aa2d
LEE HANWE <mobiledev@kakao.com>
2021-09-23 15:14:34 +0900

    Add files via upload

---
+++ GithubActionTestTests/GithubActionTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  GithubActionTestTests.swift
+//  GithubActionTestTests
+//
+//  Created by hanwe lee on 2021/09/23.
+//
+
+import XCTest
+@testable import GithubActionTest
+
+class GithubActionTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testSuccess() throws {
+        XCTAssertTrue(true)
+    }
+    
+    func testFail() throws {
+        XCTAssertTrue(false)
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-GithubActionTest/Pods-GithubActionTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GithubActionTest : NSObject
+@end
+@implementation PodsDummy_Pods_GithubActionTest
+@end
